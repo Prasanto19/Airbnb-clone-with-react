14791827f975d33109cef1a7dc435408d8f948d4
PRASHANTA <prasanto.cou@gmail.com>
2022-07-17 17:10:26 +0900

    Home Component source code added.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 /* eslint-disable react/react-in-jsx-scope */
 import Header from './components/Header/Header';
-import { BrowserRouter as Router } from 'react-router-dom';
+import Home from './components/Home/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 function App() {
   return (
     <div className="app">
       <Router>
         <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
       </Router>
     </div>
   );
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './Home.module.css';
+
+// ES7 snippets to do 'rfce'
+
+function Home() {
+  return <div className={classes.Hpme}>This is Home.</div>;
+}
+
+export default Home;
