b4a364b0bebc0027b7ee306b74ec3cfd9e244075
PRASHANTA <prasanto.cou@gmail.com>
2022-07-17 18:23:51 +0900

    Card component added.

---
+++ src/components/Home/Card/Card.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import classes from './Card.module.css';
+
+function Card({ src, title, description, price }) {
+  return (
+    <div className={classes.card}>
+      <img src={src} alt="" />
+      <div className={classes.card__info}>
+        <h2>{title}</h2>
+        <h4>{description}</h4>
+        <h3>{price}</h3>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
